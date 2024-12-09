32d43be86b6429b7140561d626c1b39769a22490
taki3210 <b1023142@fun.ac.jp>
2024-12-09 22:58:11 +0900

    pe-jisenni

--- app/src/main/java/com/example/star/Memo.java
+++ app/src/main/java/com/example/star/Memo.java
@@ -1,43 +1,34 @@
 package com.example.star;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.GridLayout;
-import android.widget.ImageButton;
-import android.widget.ImageView;
-import android.widget.Toast;
+public class Memo extends AppCompatActivity {
 
-public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_memo);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-        ImageButton toFile01 = (ImageButton) findViewById(R.id.imageButton4);
-        toFile01.setOnClickListener(new View.OnClickListener() {
+        ImageButton back02 = (ImageButton) findViewById(R.id.imageButton6);
+        back02.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, star_sky.class);
+                Intent intent = new Intent(Memo.this,star_sky.class);
                 startActivity(intent);
             }
         });
-
-
     }
-
 }
-
-
